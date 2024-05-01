21ad988637a1aeeec14af1d7d658f85017c0310c
Yasser-Aboibrahim <yasser.fayez.aboibrahim@gmail.com>
2024-05-02 00:05:32 +0300

    add modular app

---
+++ ModuleB/ModuleB.h
@@ -0,0 +1,18 @@
+//
+//  ModuleB.h
+//  ModuleB
+//
+//  Created by Yasser Aboibrahim on 01/05/2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ModuleB.
+FOUNDATION_EXPORT double ModuleBVersionNumber;
+
+//! Project version string for ModuleB.
+FOUNDATION_EXPORT const unsigned char ModuleBVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ModuleB/PublicHeader.h>
+
+
